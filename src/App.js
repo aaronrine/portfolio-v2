01666e857d9a88a8b933277407b9a0775857abec
Aaron Rine <aaronrine@gmail.com>
2021-03-07 23:05:23 -0800

    initial(App): initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import midPic from "./assets/blue-glass-highlight.jpg";
+import { useState } from "react";
 
 function App() {
+  const [burgerState, setBurgerState] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="above-fold-container">
+        <header className="main-header">
+          <svg
+            onClick={() =>
+              setBurgerState((prev) => (prev === "true" ? "false" : "true"))
+            }
+            className="hamburger"
+            viewBox="0 0 100 80"
+            width="40"
+            height="40"
+          >
+            <rect
+              direction={burgerState}
+              className="line-one"
+              width="100"
+              height="20"
+              rx="15"
+            />
+            <rect
+              direction={burgerState}
+              className="line-two"
+              y="30"
+              width="100"
+              height="20"
+              rx="15"
+            />
+            <rect
+              direction={burgerState}
+              className="line-three"
+              y="60"
+              width="100"
+              height="20"
+              rx="15"
+            />
+          </svg>
+        </header>
+        <img src={midPic} alt="blue glass geometric crystals"></img>
+      </div>
     </div>
   );
 }
