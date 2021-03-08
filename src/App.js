1d638d3e7450e894c0b4992acd74f2b025697cb5
Aaron Rine <aaronrine@gmail.com>
2021-03-08 14:00:29 -0800

    feat(app): add fontawesome icons and external links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import "./App.scss";
 import midPic from "./assets/blue-glass-highlight.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faFileDownload } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+
+const icons = [
+  {
+    icon: faGithub,
+    uri: "https://github.com/aaronrine",
+    key: 1111,
+    title: "github",
+  },
+  {
+    icon: faLinkedin,
+    uri: "https://www.linkedin.com/in/aaron-rine-1903b511a/",
+    key: 2222,
+    title: "linkedin",
+  },
+  {
+    icon: faFileDownload,
+    uri: "",
+    key: 3333,
+    title: "resume",
+  },
+];
 
 function App() {
   const [burgerState, setBurgerState] = useState(0);
@@ -42,6 +66,20 @@ function App() {
             />
           </svg>
         </header>
+        <div className="icon-list">
+          {icons.map(({ icon, uri, key, title }) => (
+            <a
+              href={uri}
+              target="_blank"
+              rel="noreferrer"
+              key={key}
+              title={title}
+            >
+              <FontAwesomeIcon icon={icon} />
+            </a>
+          ))}
+        </div>
+        <h1>Elegant solutions to practical problems.</h1>
         <img src={midPic} alt="blue glass geometric crystals"></img>
       </div>
     </div>
