f963d9c49386062f70c95c573044789076182b22
Aaron Rine <aaronrine@gmail.com>
2021-03-08 14:39:56 -0800

    feat(App): add nav bar and animations

--- src/App.js
+++ src/App.js
@@ -27,46 +27,59 @@ const icons = [
 ];
 
 function App() {
-  const [burgerState, setBurgerState] = useState(0);
+  const [burgerState, setBurgerState] = useState("false");
   return (
     <div className="App">
       <div className="above-fold-container">
         <header className="main-header">
-          <svg
-            onClick={() =>
-              setBurgerState((prev) => (prev === "true" ? "false" : "true"))
-            }
-            className="hamburger"
-            viewBox="0 0 100 80"
-            width="40"
-            height="40"
-          >
-            <rect
-              direction={burgerState}
-              className="line-one"
-              width="100"
-              height="20"
-              rx="15"
-            />
-            <rect
-              direction={burgerState}
-              className="line-two"
-              y="30"
-              width="100"
-              height="20"
-              rx="15"
-            />
-            <rect
-              direction={burgerState}
-              className="line-three"
-              y="60"
-              width="100"
-              height="20"
-              rx="15"
-            />
-          </svg>
+          <nav className="Nav" direction={burgerState}>
+            <svg
+              onClick={() =>
+                setBurgerState((prev) => (prev === "true" ? "false" : "true"))
+              }
+              className="hamburger"
+              viewBox="0 0 100 80"
+              width="40"
+              height="40"
+            >
+              <rect
+                direction={burgerState}
+                className="line-one"
+                width="100"
+                height="20"
+                rx="15"
+              />
+              <rect
+                direction={burgerState}
+                className="line-two"
+                y="30"
+                width="100"
+                height="20"
+                rx="15"
+              />
+              <rect
+                direction={burgerState}
+                className="line-three"
+                y="60"
+                width="100"
+                height="20"
+                rx="15"
+              />
+            </svg>
+            <div className="link-wrapper" direction={burgerState}>
+              <a href="#top" title="home">
+                Home
+              </a>
+              <a href="#projects" title="projects">
+                Projects
+              </a>
+              <a href="#contact" title="contact">
+                Contact
+              </a>
+            </div>
+          </nav>
         </header>
-        <div className="icon-list">
+        <div className="IconList">
           {icons.map(({ icon, uri, key, title }) => (
             <a
               href={uri}
